4ebd37349af4efc11624b8d80a940530ed564928
TimotheGonin <gonin.timothe@gmail.com>
2023-05-02 10:09:24 +0200

    ball mooves and hooked behavior

--- main.lua
+++ main.lua
@@ -12,6 +12,8 @@ ball.x = 0
 ball.y = 0
 ball.radius = 10
 ball.hooked = false
+ball.speed_x = 0
+ball.speed_y = 0
 
 function Start()
   ball.hooked = true
@@ -39,6 +41,9 @@ function love.update(dt)
   if ball.hooked == true then
     ball.x = pad.x
     ball.y = pad.y - pad.height / 2 - ball.radius
+  else
+    ball.x = ball.x + (ball.speed_x * dt)
+    ball.y = ball.y + (ball.speed_y * dt)
   end
 
 end
@@ -46,6 +51,14 @@ end
 function love.draw()
   love.graphics.rectangle('fill', pad.x - (pad.width / 2 ), pad.y - (pad.height / 2 ), pad.width, pad.height)
   love.graphics.circle('fill', ball.x, ball.y, ball.radius)
+end
+
+function love.mousepressed(x,y,n)
+  if ball.hooked == true then
+    ball.hooked = false
+    ball.speed_x = 200
+    ball.speed_y = -200
+  end
 end
 
 function love.keypressed(key)
